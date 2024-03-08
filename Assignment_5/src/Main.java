33e85491580ef20fda892c8fecd1d32e80bd33ba
Bakuri Sheklashvili <148319313+Bqko@users.noreply.github.com>
2024-03-08 21:46:35 +0400

    Update Main.java
    Added a comment

--- Assignment_5/src/Main.java
+++ Assignment_5/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-
+        // Enter the number of rows
         System.out.print("Enter the number of rows: ");
         int rows = scanner.nextInt();
 
